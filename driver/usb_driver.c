040cf26472a4d9aec196dd8865dd190b3b863beb
Debasis Sikdar <115938061+DebasisX@users.noreply.github.com>
2025-03-20 15:16:01 +0530

    added more meaningful comments for users

--- driver/usb_driver.c
+++ driver/usb_driver.c
@@ -1,13 +1,13 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/usb.h>
-#include <linux/kmod.h>  // Required for calling user-space programs
+#include <linux/kmod.h>  // user-space programs
 
 #define USB_VENDOR_ID  0x0781  // SanDisk USB
-#define USB_PRODUCT_ID 0x5567
+#define USB_PRODUCT_ID 0x5567 // remember your vendor id maybe different 
 
 static void execute_transfer_exe(void) {
-    char *argv[] = { "/home/user/transfer", NULL };  // Path to your compiled transfer executable
+    char *argv[] = { "/home/user/transfer", NULL };  // PATH to the compiled transfer executable where you compiled the transfer.c
     char *envp[] = { NULL };  // No need for PATH as no commands are required.
 
     int ret = call_usermodehelper(argv[0], argv, envp, UMH_WAIT_EXEC);
@@ -18,17 +18,17 @@ static void execute_transfer_exe(void) {
     }
 }
 
-// Device is plugged in
+// ALERT: Device is plugged in
 static int usb_probe(struct usb_interface *interface, const struct usb_device_id *id) {
     printk(KERN_INFO "Debasis USB Device Connected: Vendor=%04X, Product=%04X\n", id->idVendor, id->idProduct);
 
-    // Call the transfer executable
+    // calling the transfer executable
     execute_transfer_exe();
 
-    return 0; // Success
+    return 0; // success
 }
 
-// USB device is removed
+// USB removed
 static void usb_disconnect(struct usb_interface *interface) {
     printk(KERN_INFO "Debasis USB Device Removed\n");
 }
@@ -36,11 +36,11 @@ static void usb_disconnect(struct usb_interface *interface) {
 // Supported USB device list
 static struct usb_device_id usb_table[] = {
     { USB_DEVICE(USB_VENDOR_ID, USB_PRODUCT_ID) },
-    {}  // Terminate
+    {}  // marks termination as there are no more devices we are trying to work with
 };
 MODULE_DEVICE_TABLE(usb, usb_table);
 
-// Register USB driver
+// register driver
 static struct usb_driver usb_driver = {
     .name = "usb_gcc_driver",
     .id_table = usb_table,
